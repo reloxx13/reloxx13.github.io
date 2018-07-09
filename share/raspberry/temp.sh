ab9bce63abaf2f8b825c5bc8cc40f87905ee8635
reloxx13 <reloxx@interia.pl>
2018-07-09 20:49:10 +0200

    no message

--- share/raspberry/temp.sh
+++ share/raspberry/temp.sh
@@ -9,6 +9,11 @@ gpuTemp0=${gpuTemp0//\'/º}
 gpuTemp0=${gpuTemp0//temp=/}
 
 freq=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq)
+freq=$((freq/1000))
+
+load=$({ head -n1 /proc/stat;sleep 0.2;head -n1 /proc/stat; } | awk '/^cpu /{u=$2-u;s=$4-s;i=$5-i;w=$6-w}END{print int(0.5+100*(u+s+w)/(u+s+i+w))}')
+
+echo CPU Load: $load"%"
 echo CPU Freq: $freq"MHz"
 echo CPU Temp: $cpuTemp1"."$cpuTempM"ºC"
 echo GPU Temp: $gpuTemp0
